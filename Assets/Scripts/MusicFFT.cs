42c0939c87f214242a68b4f6780a7d33fb52cc2d
Yakin Najahi <najahi.yakin@gmail.com>
2016-12-10 22:05:41 -0500

    Possible fix for the missed prefab issue
    - Deleted the library folder
    - deletet the *.meta in the gitignore file
    - reopened the project in unity to generate the library folder again

---
+++ Assets/Scripts/MusicFFT.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFFT : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    [HideInInspector]
+    public float[] spectrum;
+
+    public int sampleSize = 256;
+    [HideInInspector]
+    public float rms;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        spectrum = new float[sampleSize];
+
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        float sum = 0;
+        for (int i = 0; i < sampleSize; i++)
+        {
+            sum += spectrum[i] * spectrum[i];
+        }
+
+        rms = Mathf.Sqrt(sum / sampleSize);
+        
+    }
+}
---
+++ Assets/UpdateScaleWithMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateScaleWithMusic : MonoBehaviour {
+
+    public float scaleMultiplier;
+    
+    private MusicFFT musicFFT;
+    private float initialScale;
+
+    // Use this for initialization
+    void Start () {
+        initialScale = transform.localScale.x;
+        musicFFT = GameObject.FindGameObjectWithTag("MusicPlayer").GetComponent<MusicFFT>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float desiredScale = initialScale + musicFFT.rms * scaleMultiplier;
+        float newScale = Mathf.Lerp(transform.localScale.x, desiredScale, Time.deltaTime * 8);
+        transform.localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
